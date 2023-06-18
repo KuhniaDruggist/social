54f85a66f2fdbcd262df44128b4d652cf3d518a4
KuhniaDruggist <druggist5059@gmail.com>
2023-06-19 00:38:43 +0300

    Update home component.

--- src/app/modules/pages/home/components/home/home.component.ts
+++ src/app/modules/pages/home/components/home/home.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { NgIf, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    NgOptimizedImage,
+    NgIf,
+  ],
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public userSrc: string | undefined;
+
+  constructor() {
+    this.userSrc = 'https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg';
+  }
 
   ngOnInit(): void {
   }
--- src/app/modules/pages/home/home.module.ts
+++ src/app/modules/pages/home/home.module.ts
@@ -2,13 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './components/home/home.component';
-
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
+  declarations: [],
   imports: [
     CommonModule,
     HomeRoutingModule
